60c640004c90a2cde136bf1ff8f1911e6c96470d
sungurovvd <sungurovvictor@gmil.com>
2023-05-02 20:49:55 +0600

    with new path

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def start_page():
      return f'main page'
 
+@app.route('/pathwithsml/<string:wrd>/<int:nmbr>')
+def nmbr_and_smbls(wrd,nmbr):
+     return f'path with {wrd} and {nmbr}'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
